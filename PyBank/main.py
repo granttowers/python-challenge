fe51181d6195c63bb1ab7a11191cbb707f56a06c
Grant Towers <grant_towers@bigpond.com>
2022-04-02 13:26:06 +0800

    Update 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,46 +1,61 @@
+# Define blank variables/lists:
+months = []
+total = []
+grtincrease = []
+grtdecrease = []
+
 # Import CSV Data
 import os
 import csv
+
 csvpath = os.path.join('Resources', 'budget_data.csv')
-
-# Definition of Variables
-#monthlyprofitloss = int(row[1]) 
-
-#--- Step 1: Calculate Total count of unique months - count added to monthcount
 with open(csvpath) as csvfile:
   csvreader = csv.reader(csvfile, delimiter=',')
   csv_header = next(csvreader)
-  monthcount = sum(1 for line in csvfile)
 
+#--- Step 1: Calculate Total count of unique months - count added to monthcount
+  #monthcount = sum(1 for line in csvfile)
 
 #--- Step 2: Calculate Net total amount of Profit/Losses for the entire period
 
-
-#profit_loss_total = sum(int(row[1]))
-#print(profit_loss_total)
-
-#--- Step 3: Calculate average of the monthly changes for the entire period
-
-
-
-#--- Step 4: Calculate The greatest increase in profits (date and amount) over the entire period
+# ForLoop: loop through each row
+  for row in csvreader:
+    months.append(row[0])
+    total.append(int(row[1]))
+    grtincrease.append(int(row[1]))
+    grtdecrease.append(int(row[1]))
 
 
+#--- Step 3: Calculate average of the monthly changes for the entire period - how does this work?
+average = [y-x for x, y in zip(total[:-1], total[1:])]
 
-#--- Step 5: Calculate the greatest increase in profits (date and amount) over the entire period
+#--- Step 4: Calculate the greatest increase in profits (date and amount) over the entire period
 
+for a, b in enumerate(grtincrease):
+  if b == max(grtincrease):
+    months_max_index = (a)
 
-#--- Step 6: Calculate the greatest decrease in losses (date and amount) over the entire period
+for c in [months[months_max_index]]:
+    grtincrease_month = c
 
+#--- Step 5: Calculate the greatest decrease in losses (date and amount) over the entire period
+
+for d, e in enumerate(grtdecrease):
+  if e == min(grtdecrease):
+    months_min_index = (d)
+
+for f in [months[months_min_index]]:
+    grtdecrease_month = f
 
 #--- Step 7: Summarise the findings into a single message 
 
 print(f"Financial Analysis")
 print(f"----------------------------")
-print(f"Total Months: {monthcount}")
-print(f"Total: ")
-
-# "Greatest Decrease in Profits: {}
+print(f"Total Months: {len(months)}")
+print(f"Total: ${sum(total)}")
+print(f"Average Change: ${round(sum(average)/len(average),2)}") 
+print(f"Greatest Increase in Profits: {grtincrease_month} ($ {max(grtincrease)})")
+print(f"Greatest Decrease in Profits: {grtdecrease_month} ($ {min(grtdecrease)})")
 
 #  Financial Analysis
 #  ----------------------------
